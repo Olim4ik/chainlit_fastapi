3e97524542e22928eae798b1c18877fcd4ae3d88
Olim4ik <oakhmadjonov.uz@gmail.com>
2025-03-13 10:09:39 +0500

    add some graphs

--- chainlit_apps/app2.py
+++ chainlit_apps/app2.py
@@ -2,4 +2,4 @@ import chainlit as cl
 
 @cl.on_message
 async def main(message: cl.Message):
-    await cl.Message(content=f"Chat Copilot: {message.content}").send()
+    await cl.Message(content=f"Chat Copilot, message in {__file__}: {message.content}").send()
